9e5f841fcfafe15f193c85155bb50f39476c52d9
matxd <matheusrcrv07@gmail.com>
2022-04-03 18:41:51 -0300

    tipos de dados, variaveis, hoisting e exercicio de fixação

---
+++ Guia JavaScript/script.js
@@ -0,0 +1,27 @@
+// JavaScript é uma linguagem com tipagem dinâmica
+
+// string -> aspas duplas, aspas simples, interpolação com template string
+const typeString = ["Matheus", "Matheus", `Matheus ${1 + 1}`];
+
+// number -> inteiro, reais, NaN
+const typeNumber = [50, 15.8, NaN];
+
+// boolean -> true, false
+const typeBoolean = [true, false];
+
+// object -> { propriedade: valor }
+const typeObjObject = { nome: "Matheus", id: 7 };
+
+// array -> []
+const typeObjArray = ["Matheus", 89];
+
+// undefined x null
+const isEqual = undefined === null; 
+
+// -------------------------------------------------
+
+
+// variaveis -> var (global-reatribuida), let (local-reatribuida), const (local-não reatribuida)
+var global = 333
+let local = 777
+const constante = 999
