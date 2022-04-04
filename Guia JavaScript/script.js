34b6ba8a95e00c21e5d047edf0d90e9958d1983c
matxd <matheusrcrv07@gmail.com>
2022-04-04 16:45:16 -0300

    functions

--- Guia JavaScript/script.js
+++ Guia JavaScript/script.js
@@ -20,8 +20,44 @@ const isEqual = undefined === null;
 
 // -------------------------------------------------
 
-
 // variaveis -> var (global-reatribuida), let (local-reatribuida), const (local-não reatribuida)
 var global = 333
 let local = 777
 const constante = 999
+
+// -------------------------------------------------
+
+// function ( sofre hoisting )
+function createPhrases(){
+    return console.log("Paciência e Persistência")
+}
+createPhrases()
+
+// function anonymous, function expression ( não sofre hoisting )
+const sum = function(a, b){
+    let total = a + b
+    return console.log(total)
+}
+sum(6, 4)
+
+// arrow function ( não sofre hoisting )
+const sayMyName = (name) => {
+    return console.log(name)
+}
+sayMyName("Matheus")
+
+// callback function
+function chooseAColor(color){
+    return color()
+}
+chooseAColor(() => { console.log("Purple") })
+
+// function constructor
+function Person(name){
+    this.name = name // this se referencia a constante mth
+    this.walk = function(){ 
+        return `${this.name} está andando` 
+    }
+}
+const mth = new Person("Matheus") // instanciando o objeto
+console.log(mth.walk())
