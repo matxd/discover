c7657de8b149f467105562bb5bf1c5d0af30b2fe
matxd <matheusrcrv07@gmail.com>
2022-04-15 15:38:30 -0300

    Páginas criadas com EJS

--- Exercicios/Node - EJS/server.js
+++ Exercicios/Node - EJS/server.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.set("view engine", "ejs")
 
-app.get("/", (request, response) => { response.render("index") })
-app.get("/sobre", (request, response) => { response.render("about") })
+app.get("/", (request, response) => { response.render("pages/index") })
+app.get("/sobre", (request, response) => { response.render("pages/about") })
 
 app.listen(8080)
